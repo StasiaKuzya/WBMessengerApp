8b4670e5b30111203a91862bd71965cc0627ac64
StasiaKuzya <kuzina-sta@mail.ru>
2024-06-05 13:14:00 +0300

    Initial Commit

---
+++ WBMessengerApp/WBMessengerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBMessengerAppApp.swift
+//  WBMessengerApp
+//
+//  Created by Анастасия on 05.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBMessengerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
